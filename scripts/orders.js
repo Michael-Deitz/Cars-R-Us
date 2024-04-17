1349c8dae494af4995d9151e526ecb85b3413beb
Michael Deitz <michaeljrdeitz@gmail.com>
2024-04-17 05:16:52 -0500

    Adding a complete button for the orders that have been placed

--- scripts/orders.js
+++ scripts/orders.js
@@ -1,21 +1,42 @@
-export const orderList = async () => {
-    const response = await fetch("http://localhost:8088/orders?_expand=paint&_expand=interior&_expand=technology&_expand=wheel")
-    const orders = await response.json()
+import { getWheels, getInteriors, getTechnologies, getPaints, getOrders } from "./database.js"
 
 
-    let ordersHTML = ""
-    const divStringArray = orders.map(
-        (order) => {
-            const orderPrice = order.paint.price + order.interior.price + order.technology.price + order.wheel.price
-            const usdPrice = orderPrice.toLocaleString("en-US", {
-                style: "currency",
-                currency: "USD"
-            })
-            return `<div>
-            <div> ${order.paint.color} car with ${order.wheel.type} wheels, ${order.interior.seat} interior, and the ${order.technology.package} for a total cost of ${usdPrice}</div>
-            </div>`
-        }
-    )
-    ordersHTML += divStringArray.join("")
-    return ordersHTML
+const paints = await getPaints()
+const interiors = await getInteriors()
+const techs = await getTechnologies()
+const wheels = await getWheels()
+
+
+export const Orders = async() => {
+    const orders = await getOrders()
+
+    return `${
+        orders.map(order => {
+            
+
+            return `<section class="order">
+                ${order.paintColor.color} car with
+                ${order.wheel.style} wheels,
+                ${order.interior.material} interior,
+                and the ${order.technology.package}
+                for a total cost of
+                ${order.totalCost}
+
+                <input type="button" name="complete" id="${order.id}" value="Complete">
+            </section>`
+
+            
+        })
+        .join("")
+    }`
+
+    
 }
+
+document.addEventListener("click", (event) => {
+    const { name, id } = event.target;
+    if (name === "complete") {
+      completeOrder(id);
+    }
+  });
+
